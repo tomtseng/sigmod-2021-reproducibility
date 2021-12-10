c2ffedf384744294743de0bc19f1f2fffb4f2869
Tom Tseng <tom.hm.tseng@gmail.com>
2021-12-10 12:02:55 -0500

    runExperiments: Create

--- prepareData.sh
+++ prepareData.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ ! -d "${HOME}/gbbs" ]
 then
     echo "Run prepareSoftware.sh before running this script."
@@ -5,5 +7,7 @@ then
 fi
 
 cd ${HOME}/gbbs
+# TODO(tomtseng): change references to master to refer to instead  a specific commit
+git checkout master
 cd benchmarks/SCAN/IndexBased/experiments
 bash download_graphs.bash
--- runExperiments.sh
+++ runExperiments.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 cd ${HOME}/gbbs
 
-# TODO run experiments
+git checkout master
+cd benchmarks/SCAN/IndexBased/experiments
+bash run_gbbs_experiments.bash
+bash run_ppscan_experiments.bash
+
+git checkout tomtseng/mkl-scan
+source ${HOME}/intel/oneapi/setvars.sh
+bash run_gbbs_mkl_experiments.bash
+
+git checkout master
+python3.6 summarize_results.py
