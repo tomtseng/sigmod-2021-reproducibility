6107720cfaccbb55c40003bdd9cfc88748d7915c
Tom Tseng <tom.hm.tseng@gmail.com>
2021-12-10 03:02:01 -0500

    prepareData: Implement the script

--- prepareData.sh
+++ prepareData.sh
@@ -1,1 +1,9 @@
-# TODO
+if [ ! -d "${HOME}/gbbs" ]
+then
+    echo "Run prepareSoftware.sh before running this script."
+    exit 1
+fi
+
+cd ${HOME}/gbbs
+cd benchmarks/SCAN/IndexBased/experiments
+bash download_graphs.bash
